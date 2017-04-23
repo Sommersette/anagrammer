4d5ea0befb8c46ec279ea1775483a2247eba34ec
TREVOR FAST <TREVORRFAST@GMAIL.com>
2017-04-22 17:43:21 -0700

    add method to remove spaces and reverse word to check if palindrome.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,6 +1,10 @@
 class String
 define_method(:anagrammer) do
-  new_word = self.downcase()
-  new_word
+  lower = self.downcase()
+    if (lower == lower.reverse())
+      # puts lower.reverse.capitalize() +" is a palindrome."
+    else
+      # puts lower.capitalize() + " is not a palindrome."
+    end
   end
 end
--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -1,9 +1,18 @@
 require('rspec')
 require('anagram')
 
-describe() do
+describe('String#anagrammer') do
 
-  it("changes any uppercase letter to lowercase") do
-    expect(("DAD").anagrammer).to(eq("dad"))
+  it("changes any uppercase letter to lowercase") do #to lowercase letter test
+    expect(("DaD").anagrammer).to(eq("dad"))
   end
+
+  it("checks if word or words are the same forwards and backwards") do #.reverse .strip
+    expect(("Racecar").anagrammer).to(eq("racecar"))
+  end
+
+  # it("Check if two letters are identical") do # <=> .sort .length and .strip!
+  #   expect(("d").anagrammer).to(eq("d"))
+  # end
+
 end
