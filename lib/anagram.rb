c4b58d4c53634bdf53fff3266e64c58c67a66f58
TREVOR FAST <TREVORRFAST@GMAIL.com>
2017-04-24 21:13:10 -0700

    add spec for checking if input is actually a word and if it is an anagram.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,35 +1,21 @@
 class String
-define_method(:anagrammer) do |second| # second is for input 2
-  enable = []
-  File.open('enable1.txt').each do |line|
-    enable << line.strip
+define_method(:anagrammer) do |second|
+
+  if (self.scan(/[aeiou]/).count >= 1 && second.scan(/[aeiou]/).count >= 1)
+  else
+    puts "How can you spell a word if you have eaten all of the vowels?"
   end
-  input1 = []
-  # input2 = []
-  #   input1.each(enable) do
-  #
-  # end
 
-
-   input1.each() do |word|
-     enable.each() do
-       if input1.include?(enable)
-         puts true
-       else
-         puts false
-       end
-     end
-
-
+  if (self.chars.sort.join() == second.chars.sort.join())
+    puts "Yummm! You have found a tasty anagram."
+  else
+    puts "Move your spoon around a little and you'll get it next time."
+  end
 
     small_letters_only = self.gsub(/([[:punct:]])/, '').downcase()
     no_space = small_letters_only.gsub(/\s+/, "") # mashes words together
     backwards = no_space.reverse() # palindrome
     backwards
-    end
+    
   end
 end
-# add.map(&:downcase)
-# use ^. and .length so it only looks for words of that length instead of going through the whole list #add information to a temp array and downcases it at the same time
-# (/[aeiou]/) all vowels in a String
-# input1 = gets.chomp()
--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -3,34 +3,34 @@ require('anagram')
 
 describe('String#anagrammer') do
 
-  # it("changes any uppercase letter to lowercase") do #to lowercase letter test
-  #   expect(("A But Tuba").anagrammer).to(eq('abuttuba'))
-  # end # refactored and output no longer passes with spaces
-  #
-  # it("removes all punctuation.") do #to lowercase letter test
-  #   expect(("A dog! A panic in a pagoda!").anagrammer).to(eq('adogapanicinapagoda'))
-  # end # refactored and no longer passes with spaces
-  #
-  # it("removes all spaces from inputted String") do
-  #   expect(("Are we not drawn onward to new era?").anagrammer).to(eq('arewenotdrawnonwardtonewera'))
-  # end
-  #
-  # it("checks if input is a palindrome") do
-  #   expect(("arewenotdrawnonwardtonewera").anagrammer).to(eq('arewenotdrawnonwardtonewera'))
-  # end
 
-  it("checks to see if input is an actual word") do
-    expect(('ruby').anagrammer('ruby')).to(eq('true'))
+  it("checks to see if two words are anagrams") do
+      expect(('dad').anagrammer('dad')).to(eq('dad'))
   end
 
+  it("checks to see if input is an actual word and has vowels") do
+    expect(('wrd').anagrammer('wrd')).to(eq('drw'))
+  end
+
+  it("removes all spaces from inputted String") do
+    expect(("Are we not drawn onward to new era?").anagrammer('')).to(eq('arewenotdrawnonwardtonewera'))
+  end
+
+  it("changes any uppercase letter to lowercase") do #to lowercase letter test
+    expect(("A But Tuba").anagrammer('')).to(eq('abuttuba'))
+  end # refactored and output no longer passes with spaces
+
+  it("removes all punctuation.") do #to lowercase letter test
+    expect(("A dog! A panic in a pagoda!").anagrammer('')).to(eq('adogapanicinapagoda'))
+  end # refactored and no longer passes with spaces
 
 
-  # it("checks if word or words are the same letter order forwards and backwards") do #.reverse & regex
-  #   expect(("A but Tuba").anagrammer).to(eq('a but tuba'))
-  # end
+  it("checks if input is a palindrome") do
+    expect(("arewenotdrawnonwardtonewera").anagrammer('')).to(eq('arewenotdrawnonwardtonewera'))
+  end
+  it("checks if input is an antigram") do
+    expect(("hello").anagrammer('bye')).to(eq('true'))
+  end
 
-  # it("Check if two letters are identical") do # <=> .sort .length and .strip!
-  #   expect(("d").anagrammer).to(eq("d"))
-  # end
 
 end
