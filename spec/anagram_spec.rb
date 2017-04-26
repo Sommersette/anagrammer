8f29911791e880feb0133570a43cca267ddd35b5
TREVOR FAST <TREVORRFAST@GMAIL.com>
2017-04-25 22:03:50 -0700

    all test are now giving nil as result.

--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -4,31 +4,30 @@ require('anagram')
 describe('String#anagrammer') do
 
   it("checks to see if input is an actual word and has vowels") do
-    expect(('wrd').anagrammer('wrd')).to(eq(nil))
+    expect(('wrd').anagrammer('wrd')).to(eq('wrd'))
   end
 
   it("checks to see if two words are anagrams") do
-      expect(('dad').anagrammer('dad')).to(eq(nil))
+      expect(('dad').anagrammer('dad')).to(eq('dad'))
   end
-
 
   it("removes all spaces from inputted String") do
-    expect(("Are we not drawn onward to new era?").anagrammer('')).to(eq(nil))
+    expect(("Are we not drawn onward to new era?").anagrammer('')).to(eq("Arewenotdrawnonwardtonewera?"))
   end
 
-  it("changes any uppercase letter to lowercase") do #to lowercase letter test
-    expect(("A But Tuba").anagrammer('')).to(eq(nil))
+  it("changes any uppercase letter to lowercase") do
+    expect(("A But Tuba").anagrammer('')).to(eq("abuttuba"))
   end # refactored and output no longer passes with spaces
 
-  it("removes all punctuation.") do #to lowercase letter test
-    expect(("A dog! A panic in a pagoda!").anagrammer('')).to(eq(nil))
+  it("removes all punctuation.") do
+    expect(("A dog! A panic in a pagoda!").anagrammer('')).to(eq('adogapanicinapagoda'))
   end # refactored and no longer passes with spaces
 
 
   it("checks if input is a palindrome") do
-    expect(("arewenotdrawnonwardtonewera").anagrammer('')).to(eq(nil))
+    expect(("arewenotdrawnonwardtonewera").anagrammer('')).to(eq('arewenotdrawnonwardtonewera'))
   end
   it("checks if input is an antigram") do
-    expect(("hello").anagrammer('bye')).to(eq(nil))
+    expect(("hello").anagrammer('bye')).to(eq(true))
   end
 end
