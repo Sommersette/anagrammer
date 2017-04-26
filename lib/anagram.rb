052fa13d8e3233710dec060d0aced961ba317878
TREVOR FAST <TREVORRFAST@GMAIL.com>
2017-04-26 07:06:58 -0700

    change methodon vowels

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,20 +1,29 @@
 class String
-define_method(:anagrammer) do |second|
-
-  if self=~(/[aeiouy]/)
-    puts "Yummers, looks like you found a tasty word!"
-    if (self.chars.sort.join() == second.chars.sort.join())
-      puts "Yummm! You have found a tasty anagram."
-      if small_letters_only = self.gsub(/([[:punct:]])/, '').downcase()
-          no_space = small_letters_only.gsub(/\s+/, "") # mashes words together
-          backwards = no_space.reverse() # palindrome
-          backwards
-        puts "is a palindrome too!"
-
+  define_method(:anagrammer) do |second|
+  if self.scan(/[aeiouy]/).join() && second.scan(/[aeiouy]/).join()
+   print "Yummers, looks like you found a tasty word!"
+   true
   else
-    puts "How can you spell a word if you have eaten all of the vowels?"
-      end
-    end
+    print "How can you spell a word if you have eaten all of the vowels?"
+    false
   end
 end
+end
+
+#   if (self.chars.sort.join() == second.chars.sort.join())
+#       puts "Yummm! You have found a tasty anagram."
+#     else
+#       puts "Not an anagram, but what a jumble of letters you have. "
+#   end
+# end
+
+  #     if small_letters_only = self.gsub(/([[:punct:]])/, '').downcase()
+  #         no_space = small_letters_only.gsub(/\s+/, "") # mashes words together
+  #         backwards = no_space.reverse() # palindrome
+  #         backwards
+  #       else
+  #         falcs
+  #     end
+  #       puts "is a palindrome too!"
+
 end
