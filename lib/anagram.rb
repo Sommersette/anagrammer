70792dd0fdf23127971d617078b9268dcfd9921c
TREVOR FAST <TREVORRFAST@GMAIL.com>
2017-04-26 21:33:07 -0700

    add and pass test for antigram

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,33 +7,33 @@ class String
     end
   end
 
-    define_method(:anagram)  do |second|
-      if (self.chars.sort() == second.chars.sort())
-          true
-        else
-          false
-      end
+  define_method(:anagram)  do |second|
+    if (self.chars.sort() == second.chars.sort())
+        true
+      else
+        false
     end
+  end
 
-    # define_method(:palindrome) do |second|
-    #   if small_letters_only = self.gsub(/(\s+&&[[:punct:]])/, '').downcase()
-    #     no_space = small_letters_only.gsub(/\s+/, "") # mashes words together
-    #     backwards = no_space.reverse() # palindrome
-    #     backwards
-    #   else
-    #     false
-    #   end
-    # end
+  define_method(:palindrome) do |second|
+    if small_letters_only = self.gsub(/([[:punct:]])/, '').downcase()
+      no_space = small_letters_only.gsub(/\s+/, "") # mashes words together
+      backwards = no_space.reverse() # palindrome
+      backwards
+    else
+      false
+    end
+  end
 
-    # define_method(:antigram) do |second|
-    #   1 = []
-    #   2 = []
-    #   self << 1
-    #   second << 2
-    #   if 1 & 2 == empty
-    #     false
-    #   else
-    #     true
-    #   end
+  define_method(:antigram) do |second|
+    first = []
+    second = []
+    self << 1
+    second << 2
+    if first & second == empty
+      false
+    else
+      true
+    end
+  end
 end
-# end
--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -22,23 +22,22 @@ describe('String#anagrammer') do
     end
   end
 
-  # it("removes all spaces from inputted String") do
-  #   expect(("Are we not drawn onward to new era?").anagrammer('')).to(eq("Arewenotdrawnonwardtonewera?"))
-  # end
-  #
-  # it("changes any uppercase letter to lowercase") do
-  #   expect(("A But Tuba").anagrammer('')).to(eq("abuttuba"))
-  # end # refactored and output no longer passes with spaces
-  #
-  # it("removes all punctuation.") do
-  #   expect(("A dog! A panic in a pagoda!").anagrammer('')).to(eq('adogapanicinapagoda'))
-  # end # refactored and no longer passes with spaces
-  #
-  #
-  # it("checks if input is a palindrome") do
-  #   expect(("arewenotdrawnonwardtonewera").anagrammer('')).to(eq('arewenotdrawnonwardtonewera'))
-  # end
-  # it("checks if input is an antigram") do
-  #   expect(("hello").anagrammer('bye')).to(eq(true))
-  # end
+  describe('String#palindrome') do
+    # it("removes all spaces from inputted String") do
+    #   expect(("Are we not drawn onward to new era?").anagrammer('')).to(eq("Arewenotdrawnonwardtonewera?"))
+    # end
+    #
+    # it("changes any uppercase letter to lowercase") do
+    #   expect(("A But Tuba").anagrammer('')).to(eq("abuttuba"))
+    # end # refactored and output no longer passes with spaces
+
+    it("removes all punctuation, changes all lettes to lowercase, and removes all spaces.") do
+      expect(("A dog! A panic in a pagoda!").anagrammer('adogapanicinapagoda')).to(eq(true))
+    end
+  end
+
+  describe('String#antigram')
+  it("checks if input is an antigram") do
+    expect(("hello").anagrammer('bye')).to(eq(true))
+  end
 end
